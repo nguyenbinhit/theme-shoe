e4c04d9c4281c572796a468a55868d027d300f66
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-04-02 18:01:45 +0700

    111

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -1,11 +1,23 @@
 function muangay() {
-    location.assign("");
+    location.assign("order.html");
 }
 
-function xemngay() {
-    location.assign("");
+function skindetail() {
+    location.assign("skin-detail.html");
+}
+
+function prodeatil() {
+    location.assign("product-detail.html");
+}
+
+function shoedetail() {
+    location.assign("shoe-detail.html");
 }
 
 function dathen() {
-    location.assign("");
+    location.assign("contact.html");
+}
+
+function cart() {
+    location.assign("order.html");
 }
